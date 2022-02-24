d383504ff2cd7125b9687ffb07dce1a583f5e66e
greekn <cheerfuls@outlook.com>
2022-02-25 00:20:08 +0800

    env

---
+++ src/main/java/cc/greekn/ChaoticLifeApplication.java
@@ -0,0 +1,23 @@
+package cc.greekn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Greekn
+ * @version 0.1
+ * @date 2022-02-24 21:42
+ * @package cc.greekn
+ * @modified Greekn
+ * @description
+ */
+@SpringBootApplication
+public class ChaoticLifeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChaoticLifeApplication.class);
+    }
+}
