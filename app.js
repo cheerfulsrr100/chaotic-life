449bbb6f81de79ba7a6e1c36fe91294f36cedec8
Greekn <cheerfuls@outlook.com>
2022-03-15 00:39:00 +0800

    feat: add jwt

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+'use strict';
 const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -14,8 +16,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const unless = (middleware, ...paths) => {
+  return function (req, res, next) {
+    const pathCheck = paths.some((path) => path === req.path);
+    if (!pathCheck) middleware(req, res, next);
+    next();
+  };
+};
+
+const tokenRegex = /Bearer\s([A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)/;
+
+const verifyToken = function (req, res, next) {
+  const authHeader = req.headers.authorization;
+  const jwtToken = authHeader.match(tokenRegex).pop();
+  if (jwtToken !== undefined) {
+    try {
+      jwt.verify(jwtToken, 'greekn');
+      req.user = {
+        id: 1,
+        name: 'test',
+      };
+      return [req, res];
+    } catch (err) {
+      res.json({ msg: 'token invalid' });
+    }
+  }
+};
+
+app.use(unless(verifyToken, '/user/login', '/user/register'));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
+app.use(
+  '/test',
+  express.Router().get('', function (req, res, next) {
+    res.json({ msg: 'ok' });
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -30,7 +66,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ msg: 'error' });
 });
 
 module.exports = app;
